5d205658fd8c641368d32989b7dc270826e47207
adel <adeelguitoun@gmail.com>
2024-11-23 09:58:48 +0100

    Setup tailwind config and style guide css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,70 @@
-import type { Config } from "tailwindcss";
+/* eslint-disable @typescript-eslint/no-require-imports */
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          500: '#ff7000',
+          100: '#fff1e6',
+        },
+        dark: {
+          100: '#000',
+          200: '#0f1117',
+          300: '#151821',
+          400: '#212734',
+          500: '#3f4354',
+        },
+        light: {
+          900: '#fff',
+          850: '#fdfdfd',
+          800: '#f4f6f8',
+          700: '#dce3f1',
+          500: '#7b8ec8',
+          400: '#858ead',
+        },
+        'light-blue': '#1da1f2',
+      },
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        spaceGrotesk: ['var(--font-spaceGrotesk)'],
+      },
+      boxShadow: {
+        'light-100':
+          '0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184,184,184,0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)',
+        'light-200': '10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
+        'light-300': '-10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
+        'dark-100': '0px 2px 10px 0px rgba(46, 52, 56, 0.10)',
+        'dark-200': '2px 0px 20px 0px rgba(39, 36, 36, 0.04)',
+      },
+      backgroundImage: {
+        'auth-dark': "url('/assets/images/auth-dark.png')",
+        'auth-light': "url('/assets/images/auth-light.png')",
+      },
+      screens: {
+        xs: '420px',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0px' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0px' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 } satisfies Config;
