a490bf8a9aa97edfb666b185ae6dcb7960d93dec
Adel Guitoun <adeelguitoun@gmail.com>
2024-11-27 16:50:57 +0100

    Revert "Created the ask-question page"
    This reverts commit 0e3e70a5c6a0ef950b21a395d1f72f4390440e13.

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,24 +1,4 @@
-import Question from '@/components/forms/Question';
-import { getUserById } from '@/lib/actions/user.action';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-
-const AskQuestion = async () => {
-  // const { userId } = await auth();
-
-  const userId = 'clerk12345';
-
-  if (!userId) redirect('/sign-in');
-
-  const mongoUser = await getUserById({ userId });
-
-  return (
-    <div>
-      <div className="h1-bold text-dark100_light900">Ask a question</div>
-      <div className="mt-9">
-        <Question mongoUserId={mongoUser._id.toString()} />
-      </div>
-    </div>
-  );
+const AskQuestion = () => {
+  return <div>AskQuestion</div>;
 };
 export default AskQuestion;
