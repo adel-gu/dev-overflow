982cc28f86a3b9a12e1de02d4dca7be15e6ea886
adel <adeelguitoun@gmail.com>
2024-11-28 16:48:06 +0100

    Create get all users action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -4,6 +4,7 @@ import User from '@/database/user.model';
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   GetUserByIdParams,
   UpdateUserParams,
 } from './shared.types';
@@ -68,6 +69,21 @@ export async function deleteUser(params: DeleteUserParams) {
     const deletedUser = await User.findByIdAndDelete(user._id);
 
     return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // TODO: setup filtering, pagination, searchQuery
+    // const { page = 1, pageSize = 1, filter, searchQuery } = params;
+    const users = await User.find({}).sort({ joinedAt: -1 });
+
+    return { users };
   } catch (error) {
     console.log(error);
     throw error;
