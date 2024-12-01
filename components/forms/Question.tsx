7afa0f7f77ac0a8d7093c39669baaedc904c397d
adel <adeelguitoun@gmail.com>
2024-12-01 09:39:18 +0100

    Update editor theme

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,6 +21,7 @@ import { Badge } from '../ui/badge';
 import Image from 'next/image';
 import { createQuestion } from '@/lib/actions/question.action';
 import { useRouter } from 'next/navigation';
+import { useTheme } from '@/context/Theme';
 
 interface QuestionProps {
   mongoUserId: string;
@@ -29,6 +30,7 @@ interface QuestionProps {
 const type: any = 'create';
 
 const Question = ({ mongoUserId }: QuestionProps) => {
+  const { mode } = useTheme();
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
@@ -170,6 +172,8 @@ const Question = ({ mongoUserId }: QuestionProps) => {
                       'codesample bold italic forecolor | alignleft aligncenter ' +
                       'alignright alignjustify | bullist numlist',
                     content_style: 'body { font-family:Inter; font-size:16px }',
+                    skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
+                    content_css: mode === 'dark' ? 'dark' : 'light',
                   }}
                 />
               </FormControl>
