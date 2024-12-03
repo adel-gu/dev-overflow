e8fe03e82754284c9a5462ec174b182b737dc3c1
adel <adeelguitoun@gmail.com>
2024-12-03 10:46:25 +0100

    Fetch hot questions and tags

--- components/shared/sidebars/RightSideBar.tsx
+++ components/shared/sidebars/RightSideBar.tsx
@@ -1,24 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import TagCard from '../cards/TagCard';
+import { getHotQuestions } from '@/lib/actions/question.action';
+import { getHotTags } from '@/lib/actions/tag.actions';
 
-const hotQuestions = [
-  { _id: '1', title: 'How to create a custom hook in React?' },
-  { _id: '2', title: 'How to use React Query?' },
-  { _id: '3', title: 'How to use Redux?' },
-  { _id: '4', title: 'How to use React Router?' },
-  { _id: '5', title: 'How to use React Context?' },
-];
+const RightSideBar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getHotTags();
 
-const popularTags = [
-  { _id: '1', name: 'react', questions: 100 },
-  { _id: '2', name: 'javascript', questions: 200 },
-  { _id: '3', name: 'typescript', questions: 150 },
-  { _id: '4', name: 'nextjs', questions: 50 },
-  { _id: '5', name: 'react-query', questions: 75 },
-];
-
-const RightSideBar = () => {
   return (
     <section
       className="background-light900_dark200 light-border
@@ -30,7 +19,7 @@ const RightSideBar = () => {
           {hotQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="flex-between cursor-pointer gap-7"
             >
               <p className="body-medium text-dark500_light700">
@@ -55,7 +44,7 @@ const RightSideBar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.questions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
