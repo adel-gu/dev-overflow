33ec01e18f3c70b28cad01ac5c21cf2829b1e562
adel <adeelguitoun@gmail.com>
2024-12-05 16:22:33 +0100

    Implemented badging system

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -16,6 +16,8 @@ import {
   UpdateUserParams,
 } from './shared.types';
 import Answer from '@/database/answer.model';
+import { BadgeCriteriaType } from '@/types';
+import { assignBadges } from '../utils';
 
 export async function getUserById(params: GetUserByIdParams) {
   try {
@@ -242,7 +244,43 @@ export async function getUserInfo(params: GetUserByIdParams) {
     const totalQuestions = await Question.countDocuments({ author: user._id });
     const totalAnswers = await Answer.countDocuments({ author: user._id });
 
-    return { user, totalQuestions, totalAnswers };
+    const [questionUpvotes] = await Question.aggregate([
+      { $match: { author: user._id } },
+      { $project: { _id: 0, upvotes: { $size: '$upvotes' } } },
+      { $group: { _id: null, totalUpvotes: { $sum: '$upvotes' } } },
+    ]);
+
+    const [answerUpvotes] = await Answer.aggregate([
+      { $match: { author: user._id } },
+      { $project: { _id: 0, upvotes: { $size: '$upvotes' } } },
+      { $group: { _id: null, totalUpvotes: { $sum: '$upvotes' } } },
+    ]);
+
+    const [questionViews] = await Question.aggregate([
+      { $match: { author: user._id } },
+      { $group: { _id: null, totalViews: { $sum: '$views' } } },
+    ]);
+
+    const criteria = [
+      { type: 'QUESTION_COUNT' as BadgeCriteriaType, count: totalQuestions },
+      { type: 'ANSWER_COUNT' as BadgeCriteriaType, count: totalAnswers },
+      {
+        type: 'QUESTION_UPVOTES' as BadgeCriteriaType,
+        count: questionUpvotes?.totalUpvotes ?? 0,
+      },
+      {
+        type: 'ANSWER_UPVOTES' as BadgeCriteriaType,
+        count: answerUpvotes?.totalUpvotes ?? 0,
+      },
+      {
+        type: 'TOTAL_VIEWS' as BadgeCriteriaType,
+        count: questionViews?.totalViews ?? 0,
+      },
+    ];
+
+    const badgeCounts = assignBadges({ criteria });
+
+    return { user, totalQuestions, totalAnswers, badgeCounts };
   } catch (error) {
     console.log(error);
     throw error;
